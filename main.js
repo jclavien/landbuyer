b024c964802966401656b66e7da6008ee458c6d1
abdelaz3r <gil.clavien@gmail.com>
2018-12-11 13:52:35 +0100

    Refactoring

--- main.js
+++ main.js
@@ -38,12 +38,11 @@ const connection = oanda.createContext()
 // Ici on pose des variables générales (donc à l'extérieur du worker). En JS on
 // à pas besoin de déclarer tout les variables au début du fichier comme en C.
 // Et c'est pas une pratique qui se fait.
+let iteration = 0
 
-var TakeProfitList = new Array();
-var LimitOrderList = new Array();
-var OrderToBePlaced = new Array();
-var SearchedOrder = 0;
-let round = 0
+function round(number) {
+  return (Math.round(number * 100) / 100)
+}
 
 
 // ---------->   WORKER     <-------------------------------------------------------------------------------
@@ -52,15 +51,29 @@ let round = 0
 // Le code qui travail vraiment. Tout ce qui n'est pas fait/à faire toutes les
 // X secondes sera mis dehors, avant ou après.
 let worker = setInterval(() => {
-  // On affiche à quel round on est (c'est pas hyper important) et ça pourra
+  // On affiche à quel iteration on est (c'est pas hyper important) et ça pourra
   // être supprimé par la suite. C'est pratique pour le développement.
   // Tu peux voir qu'ici on utilise une chaîne de caractère spéciale:
   // `du text ${uneVariable}`
   // On appelle ça l'interpolation, ça permet de mettre des variables directement
   // dans des strings.
-  round += 1
-  console.log(`Round #${round}`)
+  iteration += 1
+  console.log(`Round #${iteration}`)
   console.log('----');
+
+  // J'ai déplacé tes variables ici. Quelques remarques.
+  // 1. Comme tu les vides à la fin, le mieux est de les affecter directement ici
+  //    Ce sont des variables qui ne sont utilisées que dans cette boucle, et
+  //    surtout on a pas besoin de garder les infos après la fin de la boucle.
+  // 2. Pense à utiliser "let" au lieu de "var". Les deux marchent, mais let est
+  //    la bonne manière de faire. Si tu trouves des tutos ou des exemples de code
+  //    qui écrivent "var", c'est parce que c'est vieux.
+  // 3. La convention, en javascript, c'est de faire des variables en camelCase
+  //    (donc sans majuscule au début).
+  let takeProfitList = new Array();
+  let limitOrderList = new Array();
+  let orderToBePlacedList = new Array();
+  let searchedOrder = 0;
 
   // On récupère les infos du compte
   connection.account.get(options.activeAccount, response => {
@@ -71,85 +84,80 @@ let worker = setInterval(() => {
 
     // On test si il y a des pendingOrders, 
     if (account.pendingOrderCount > 0) {
-      // On rempli un tableau nommée TakeProfitList et un autre nommé LimitOrderList qui contient le price de ces ordres ouverts
+      // On rempli un tableau nommée takeProfitList et un autre nommé limitOrderList qui contient le price de ces ordres ouverts
       for (let order of account.orders) {
-		  if (order.type == 'TAKE_PROFIT') { //si l'ordre est de type Take_Profit
-			TakeProfitList.push(Math.round((order.price)*100)/100);// on le met dans notre tableau 
+        if (order.type == 'TAKE_PROFIT') { //si l'ordre est de type Take_Profit
+          takeProfitList.push(round(order.price)) // on le met dans notre tableau 
+        } else if (order.type == 'MARKET_IF_TOUCHED') {
+          limitOrderList.push(round(order.price))
+        }
       }
-	  if (order.type == 'MARKET_IF_TOUCHED') {
-		LimitOrderList.push(Math.round((order.price)*100)/100);
-	  }
-										}
-	// On trie nos tableau dans l'ordre croissant
-		  TakeProfitList.sort()
-		  LimitOrderList.sort()
-	// On affiche nos tableaux
-			console.log('TAKE PROFIT ORDERS');
-		    console.log(TakeProfitList);
-			console.log('----');
-			console.log('LIMIT ORDERS');
-		    console.log(LimitOrderList);
-			console.log('----');
       
-// On renseigne la variable highTradeValue qui est le niveau de prix de la position ouverte la plus haute. Ce prix est calculé par rapport au take profit le plus haut.
-	let highTradeValue = Math.round((Math.max(...TakeProfitList) - distOnTakeProfit)*100)/100;	
-// Idem pour le trade ouvert le plus bas 
-	let lowTradeValue = Math.round((Math.min(...TakeProfitList) - distOnTakeProfit)*100)/100;	
+      // On trie nos tableau dans l'ordre croissant
+      takeProfitList.sort()
+      limitOrderList.sort()
+      
+      // On affiche nos tableaux
+      console.log('TAKE PROFIT ORDERS');
+      console.log(takeProfitList);
+      console.log('----');
+      console.log('LIMIT ORDERS');
+      console.log(limitOrderList);
+      console.log('----');
+      
+      // On renseigne la variable highTradeValue qui est le niveau de prix de la position ouverte la plus haute. Ce prix est calculé par rapport au take profit le plus haut.
+      let highTradeValue = round(Math.max(...takeProfitList) - distOnTakeProfit)
+      // Idem pour le trade ouvert le plus bas 
+      let lowTradeValue = round(Math.min(...takeProfitList) - distOnTakeProfit)
 
-// On affiche les deux valeurs ainsi calculées
+      // On affiche les deux valeurs ainsi calculées
       console.log(`high trade: ${highTradeValue}`)
       console.log(`low trade: ${lowTradeValue}`)
       console.log('----')
-	 
-// On rempli un tableau avec les niveau de prix des ordres que l'on devrait ouvrir
+   
+      // On rempli un tableau avec les niveau de prix des ordres que l'on devrait ouvrir
       for (let i = 1; i < nbOrders; i++) { 
-		   var SearchedOrder= Math.round((i*distBetweenPosition/100+highTradeValue)*100)/100; // le *100/100 est pour arrondir à 2 chiffres après la virgule
-		   if (LimitOrderList.includes(SearchedOrder) == false){ //si notre tableau LimitOrderList ne contient pas encore l'ordre recherché
-		   OrderToBePlaced.push(SearchedOrder); // alors on l'ajoute au tableau OrderToBePlaced
-		   } // J'ai ici un probleme ici puisque le programme pousse tous les ordres recherchés dans le tableau, meme si ca existe deja dans le tableau LimitOrder	   
-	  }
+        searchedOrder= round(i * distBetweenPosition / 100 + highTradeValue)
+        
+        if (limitOrderList.includes(searchedOrder) == false){ // si notre tableau limitOrderList ne contient pas encore l'ordre recherché
+          orderToBePlacedList.push(searchedOrder) // alors on l'ajoute au tableau orderToBePlacedList
+        } // J'ai ici un probleme ici puisque le programme pousse tous les ordres recherchés dans le tableau, meme si ca existe deja dans le tableau LimitOrder     
+      }
 
       // Idem pour les ordres inférieurs
       for (let i = 1; i < nbOrders; i++) {
-		  var SearchedOrder= Math.round((lowTradeValue-i*distBetweenPosition/100)*100)/100;
-		   if (LimitOrderList.includes(SearchedOrder) == false){
-		   OrderToBePlaced.push(SearchedOrder);
-		   }
-       
+        searchedOrder= round(lowTradeValue - i * distBetweenPosition / 100)
+
+        if (limitOrderList.includes(searchedOrder) == false){
+          orderToBePlacedList.push(searchedOrder);
+        }
       }
-	   console.log('ORDERS TO BE PLACED')
-	  console.log(OrderToBePlaced)
-	  console.log('----');
-	  
-	// Une fois le tableau des OrderToBePlaced rempli on place les ordres avec comme paramètre les prix ainsi obtenus, le montant (const) et le takeprofit (const)
-	 
-// J'ai trouvé ca online : 
-/**
-* @method createOrder
-* @param {String} accountId Required.
-* @param {Object} order
-* @param {String} order.instrument Required. Instrument to open the order on.
-* @param {Number} order.units Required. The number of units to open order for.
-* @param {String} order.side Required. Direction of the order, either ‘buy’ or ‘sell’.
-* @param {String} order.type Required. The type of the order ‘limit’, ‘stop’, ‘marketIfTouched’ or ‘market’.
-* @param {String} order.expiry Required. If order type is ‘limit’, ‘stop’, or ‘marketIfTouched’. The value specified must be in a valid datetime format.
-* @param {String} order.price Required. If order type is ‘limit’, ‘stop’, or ‘marketIfTouched’. The price where the order is set to trigger at.
-* @param {Number} order.lowerBound Optional. The minimum execution price.
-* @param {Number} order.upperBound Optional. The maximum execution price.
-* @param {Number} order.stopLoss Optional. The stop loss price.
-* @param {Number} order.takeProfit Optional. The take profit price.
-* @param {Number} order.trailingStop Optional The trailing stop distance in pips, up to one decimal place.
-* @param {Function} callback
-*/
-//	create.order(AccoundID: le token le numero de compte?,  ccyPair, positionAmount, buy, orderSide, MARKET_IF_TOUCHED, GTC, Le prix du OrderToBePlaced, vide, vide, vide,
-//			Le prix du OrderToBePlaced + distOnTakeProfit, vide, Callback : c'est quoi?) 
-	
-	
-		
-	 //Réinitialisation des tableaux
-	 TakeProfitList = []; 
-	 LimitOrderList = [];
-	 OrderToBePlaced = [];
+      
+      console.log('ORDERS TO BE PLACED')
+      console.log(orderToBePlacedList)
+      console.log('----')
+
+      // Une fois le tableau des orderToBePlacedList rempli on place les ordres avec comme paramètre les prix ainsi obtenus, le montant (const) et le takeprofit (const)
+      // J'ai trouvé ca online : 
+      /**
+      * @method createOrder
+      * @param {String} accountId Required.
+      * @param {Object} order
+      * @param {String} order.instrument Required. Instrument to open the order on.
+      * @param {Number} order.units Required. The number of units to open order for.
+      * @param {String} order.side Required. Direction of the order, either ‘buy’ or ‘sell’.
+      * @param {String} order.type Required. The type of the order ‘limit’, ‘stop’, ‘marketIfTouched’ or ‘market’.
+      * @param {String} order.expiry Required. If order type is ‘limit’, ‘stop’, or ‘marketIfTouched’. The value specified must be in a valid datetime format.
+      * @param {String} order.price Required. If order type is ‘limit’, ‘stop’, or ‘marketIfTouched’. The price where the order is set to trigger at.
+      * @param {Number} order.lowerBound Optional. The minimum execution price.
+      * @param {Number} order.upperBound Optional. The maximum execution price.
+      * @param {Number} order.stopLoss Optional. The stop loss price.
+      * @param {Number} order.takeProfit Optional. The take profit price.
+      * @param {Number} order.trailingStop Optional The trailing stop distance in pips, up to one decimal place.
+      * @param {Function} callback
+      */
+      // create.order(AccoundID: le token le numero de compte?,  ccyPair, positionAmount, buy, orderSide, MARKET_IF_TOUCHED, GTC, Le prix du orderToBePlacedList, vide, vide, vide,
+      // Le prix du orderToBePlacedList + distOnTakeProfit, vide, Callback : c'est quoi?) 
     }
   })
 }, workerInterval)
