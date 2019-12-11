34d3a90e757d067a3085ca91ea7cf31ce15a283b
Jerome Clavien <jclavien@gmail.com>
2019-12-11 16:22:48 +0100

    ajout de NZDUSD

--- main.js
+++ main.js
@@ -71,6 +71,13 @@ const instruments = [
     distOnTakeProfit: -0.04,
     distBetweenPosition: 1,
     nbOrders: 50,
+  }, {
+    ccyPair: 'NZD_JPY',
+    roundDecimalNumber: 4,
+    positionAmount: 10,
+    distOnTakeProfit: 0.0010,
+    distBetweenPosition: 0.01,
+    nbOrders: 50,
   }
 ]
 
