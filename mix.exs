7c54ee4fdbb4df32c2935b8fa4b9fa92053b5451
abdelaz3r <gil.clavien@gmail.com>
2023-12-12 15:33:51 +0100

    Clean up mix.exs

--- mix.exs
+++ mix.exs
@@ -33,7 +33,6 @@ defmodule Landbuyer.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      #{:bcrypt_elixir, "~> 2.0"},
       {:phoenix, "~> 1.7.7"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
@@ -46,17 +45,14 @@ defmodule Landbuyer.MixProject do
       {:floki, ">= 0.30.0", only: :test},
       {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
-      #{:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      #{:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:lucide_live_view, git: "https://github.com/abdelaz3r/lucide_live_view.git", tag: "v1.1"},
-      #{:openai, "~> 0.5.2"}
+      {:lucide_live_view, git: "https://github.com/abdelaz3r/lucide_live_view.git", tag: "v1.1"}
     ]
   end
 
