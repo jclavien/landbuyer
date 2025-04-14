469f70314bdcd9ca5e6ad484f87f620b29c6eacf
jclavien <48794833+jclavien@users.noreply.github.com>
2025-04-14 10:56:26 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Landbuyer.MixProject do
   def project do
     [
       app: :landbuyer,
-      version: "1.0.17",  # <-- Version mise à jour
+      version: "1.0.18",  # <-- Version mise à jour
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
